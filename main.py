d5eec1de7c5db4179efe95c17c6abc5eb255d851
Kazu-matu <kazufumi0707@gmail.com>
2021-04-17 18:17:40 +0900

    '1st'

---
+++ main.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from PLI import Image
+
+st.title('Streamlit 超入門')
+st.write('DataFrame')
+
+df = pd.DataFrame(
+    np.random.rand(20,3),
+    columns=['a','b','c']
+)
+
+st.dataframe(df.style.highlight_max(axis=0))
+
+# st.table(df.style.highlight_max(axis=0))
+
+# """
+# # 章
+# ## 節
+# ### 項
+# """
+
+# '''
+# import streamlit as st
+# import numpy as np
+# import pandas as pd
+
+# '''
+#st.line_chart(df)
+
+#st.altair_chart(df)
+st.bar_chart(df)
+
+
+
